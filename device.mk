4b024b0c4d373f6bad859c3353cccbb81dbf6568
GSandeep96 <14311A0431@sreenidhi.edu.in>
2017-10-08 16:31:15 +0530

    P2a42 : Include libbt-vendor

--- device.mk
+++ device.mk
@@ -70,6 +70,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
      android.hardware.bluetooth@1.0-impl \
      android.hardware.bluetooth@1.0-service \
+     libbt-vendor
 
 # ANT+
 PRODUCT_PACKAGES += \
