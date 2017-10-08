ca2fc7a3192f858f591ef36875e216a17037907a
GSandeep96 <14311A0431@sreenidhi.edu.in>
2017-10-08 17:03:33 +0530

    P2a42 : Add Renderscipt HAL implementation for Oreo

--- device.mk
+++ device.mk
@@ -137,7 +137,11 @@ PRODUCT_PACKAGES += \
      android.hardware.graphics.allocator@2.0-impl \
      android.hardware.graphics.allocator@2.0-service \
      android.hardware.graphics.composer@2.1-impl \
-     android.hardware.graphics.mapper@2.0-impl \
+     android.hardware.graphics.mapper@2.0-impl 
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
 
 # Ebtables
 PRODUCT_PACKAGES += \
