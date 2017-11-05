9e81e1f74b84e3094cf546a3da729ca86fe46373
GSandeep96 <14311A0431@sreenidhi.edu.in>
2017-11-05 22:18:42 +0530

    P2a42 : Use TARGET_FS_CONFIG_GEN and switch from using android_filesystem_config.h.

---
+++ config.fs
@@ -0,0 +1,29 @@
+[system/bin/wcnss_filter]
+mode: 00755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
+
+[system/bin/cnss-daemon]
+mode: 00755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BIND_SERVICE
+
+[system/bin/pm-service]
+mode: 00755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BIND_SERVICE
+
+[system/bin/imsdatadaemon]
+mode: 00755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BIND_SERVICE
+
+[system/bin/ims_rtp_daemon]
+mode: 00755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: BIND_SERVICE
