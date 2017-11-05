e5ac9e2ddb7fc88d9aed631e5af6306ec68288b6
GSandeep96 <14311A0431@sreenidhi.edu.in>
2017-11-05 18:04:30 +0530

    P2a42 : Add Gatekeeper HAL implementation for Oreo

--- device.mk
+++ device.mk
@@ -169,6 +169,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8953 \
