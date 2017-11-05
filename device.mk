8af6cc38eac55853bb276ffde2b0245c3181c733
GSandeep96 <14311A0431@sreenidhi.edu.in>
2017-11-05 18:08:53 +0530

    P2a42 : Add USB HAL implementation for Oreo

--- device.mk
+++ device.mk
@@ -322,6 +322,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
      android.hardware.vibrator@1.0-impl
 
+# USB HAL
+PRODUCT_PACKAGES += \
+     android.hardware.usb@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     libqsap_sdk \
