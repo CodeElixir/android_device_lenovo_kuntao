13fa34fb7f1958253cc643db1ed41ae8fbae2492
GSandeep96 <14311A0431@sreenidhi.edu.in>
2017-11-05 17:59:41 +0530

    P2a42 : Add DRM HAL implementation for Oreo

--- device.mk
+++ device.mk
@@ -198,6 +198,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# DRM
+ PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+    
 # Media 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
