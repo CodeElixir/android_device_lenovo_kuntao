8fd517ccc76a74018aac958c556c4d2b4b813e93
GSandeep96 <14311A0431@sreenidhi.edu.in>
2017-11-07 17:18:03 +0530

    P2a42 : Install sec_config to /vendor/etc

--- device.mk
+++ device.mk
@@ -234,7 +234,7 @@ PRODUCT_COPY_FILES += \
 
 # IRSC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
 # DRM
  PRODUCT_PACKAGES += \
