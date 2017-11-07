11d1870482d353bfc89440b05c97b83b5cc99299
GSandeep96 <14311A0431@sreenidhi.edu.in>
2017-11-07 17:15:42 +0530

    P2a42 : Add android.hardware.drm@1.0-service to product packages

--- device.mk
+++ device.mk
@@ -238,7 +238,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
  PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
     
 # Media 
 PRODUCT_COPY_FILES += \
