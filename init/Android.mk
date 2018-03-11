caca3fd5f5df042a00270f3b5ffe28fe0db71e69
GSandeep <14311A0431@sreenidhi.edu.in>
2018-03-12 02:12:45 +0530

    P2a42 : Update init extension

--- init/Android.mk
+++ init/Android.mk
@@ -18,9 +18,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := system/core/init
-LOCAL_CPPFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+
 LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
+LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_kuntao.cpp
 LOCAL_MODULE := libinit_kuntao
 
--- init/init_kuntao.cpp
+++ init/init_kuntao.cpp
@@ -1,7 +1,6 @@
 /*
    Copyright (c) 2016, The Linux Foundation. All rights reserved.
-   Copyright (c) 2017, The LineageOS Project
-
+   Copyright (c) 2018, The LineageOS Project
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -31,11 +30,17 @@
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 
-#include "vendor_init.h"
-#include "property_service.h"
-#include "util.h"
-
+#include <android-base/file.h>
 #include <android-base/properties.h>
+#include <android-base/strings.h>
+
+#include "property_service.h"
+#include "vendor_init.h"
+
+using android::base::GetProperty;
+using android::base::ReadFileToString;
+using android::base::Trim;
+using android::init::property_set;
 
 char const *heapstartsize;
 char const *heapgrowthlimit;
@@ -43,8 +48,6 @@ char const *heapsize;
 char const *heapminfree;
 char const *heapmaxfree;
 char const *large_cache_height;
-
-using android::base::SetProperty;
 
 void check_device()
 {
@@ -76,22 +79,22 @@ void vendor_load_properties()
 {
     check_device();
 
-    SetProperty("dalvik.vm.heapstartsize", heapstartsize);
-    SetProperty("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    SetProperty("dalvik.vm.heapsize", heapsize);
-    SetProperty("dalvik.vm.heaptargetutilization", "0.75");
-    SetProperty("dalvik.vm.heapminfree", heapminfree);
-    SetProperty("dalvik.vm.heapmaxfree", heapmaxfree);
+    property_set("dalvik.vm.heapstartsize", heapstartsize);
+    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_set("dalvik.vm.heapsize", heapsize);
+    property_set("dalvik.vm.heaptargetutilization", "0.75");
+    property_set("dalvik.vm.heapminfree", heapminfree);
+    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
 
-    SetProperty("ro.hwui.texture_cache_size", "72");
-    SetProperty("ro.hwui.layer_cache_size", "48");
-    SetProperty("ro.hwui.r_buffer_cache_size", "8");
-    SetProperty("ro.hwui.path_cache_size", "32");
-    SetProperty("ro.hwui.gradient_cache_size", "1");
-    SetProperty("ro.hwui.drop_shadow_cache_size", "6");
-    SetProperty("ro.hwui.texture_cache_flushrate", "0.4");
-    SetProperty("ro.hwui.text_small_cache_width", "1024");
-    SetProperty("ro.hwui.text_small_cache_height", "1024");
-    SetProperty("ro.hwui.text_large_cache_width", "2048");
-    SetProperty("ro.hwui.text_large_cache_height", large_cache_height);
+    property_set("ro.hwui.texture_cache_size", "72");
+    property_set("ro.hwui.layer_cache_size", "48");
+    property_set("ro.hwui.r_buffer_cache_size", "8");
+    property_set("ro.hwui.path_cache_size", "32");
+    property_set("ro.hwui.gradient_cache_size", "1");
+    property_set("ro.hwui.drop_shadow_cache_size", "6");
+    property_set("ro.hwui.texture_cache_flushrate", "0.4");
+    property_set("ro.hwui.text_small_cache_width", "1024");
+    property_set("ro.hwui.text_small_cache_height", "1024");
+    property_set("ro.hwui.text_large_cache_width", "2048");
+    property_set("ro.hwui.text_large_cache_height", large_cache_height);
 }
