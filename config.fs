f1c825e817ae0e7d4a20aa9076dbf30aead94ede
GSandeep <14311A0431@sreenidhi.edu.in>
2018-03-12 02:03:59 +0530

    P2a42 : Update config.fs Add AID_QCOM_DIAG, AID_RFS, and AID_RFS_SHARED Add entries from CAF

--- config.fs
+++ config.fs
@@ -1,29 +1,132 @@
-[system/bin/wcnss_filter]
-mode: 00755
+[AID_QCOM_DIAG]
+value:2950
+
+[AID_QTI_DIAG]
+value:2901
+
+[AID_RFS]
+value:2951
+
+[AID_RFS_SHARED]
+value:2952
+
+[system/vendor/bin/wcnss_filter]
+mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
 
-[system/bin/cnss-daemon]
-mode: 00755
+[system/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
+
+
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: BIND_SERVICE
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/cnss-daemon]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
 
 [system/bin/pm-service]
-mode: 00755
+mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: BIND_SERVICE
+caps: NET_BIND_SERVICE
 
-[system/bin/imsdatadaemon]
-mode: 00755
+[system/vendor/bin/pd-mapper]
+mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: BIND_SERVICE
+caps: NET_BIND_SERVICE
 
-[system/bin/ims_rtp_daemon]
-mode: 00755
+[system/bin/pd-mapper]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/ims_rtp_daemon]
+mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
-caps: BIND_SERVICE
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: WAKE_ALARM
+
+[system/vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
+
+[vendor/bin/slim_daemon]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/slim_daemon]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/xtwifi-client]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE BLOCK_SUSPEND
+
+[system/vendor/bin/mm-qcamera-daemon]
+mode: 0700
+user: AID_CAMERA
+group: AID_SHELL
+caps: SYS_NICE
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[dsp/]
+mode: 0771
+user: AID_MEDIA
+group: AID_MEDIA
+caps: 0
