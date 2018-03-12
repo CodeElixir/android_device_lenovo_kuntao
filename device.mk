e2bf0a445d05838e86c3cd681f9e9802e690995c
GSandeep <14311A0431@sreenidhi.edu.in>
2018-03-12 22:30:40 +0530

    P2a42 : Build OSS camera HAL

--- device.mk
+++ device.mk
@@ -130,12 +130,16 @@ PRODUCT_COPY_FILES += \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
- # Camera HAL
+# Camera HAL
 PRODUCT_PACKAGES += \
-     camera.device@1.0-impl \
-     camera.device@3.2-impl \
-     android.hardware.camera.provider@2.4-impl \
-     android.hardware.camera.provider@2.4-service
+	camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
+    camera.msm8953 \
+    libmm-qcamera \
+    Snap \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
  # Display
 PRODUCT_PACKAGES += \
