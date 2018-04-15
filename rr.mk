3c7e47ce503479f9547fef8e8fe057a1fd5740a0
GSandeep <14311A0431@sreenidhi.edu.in>
2018-04-15 11:02:21 +0530

    Changes done for RR

--- rr.mk
+++ rr.mk
@@ -16,14 +16,16 @@
 
 $(call inherit-product, device/lenovo/kuntao/full_kuntao.mk)
 
-# Inherit some common AOSP-OMS stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit RR's custom product configuration
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-# Boot Animation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_kuntao
+PRODUCT_NAME := lineage_kuntao
 BOARD_VENDOR := Lenovo
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	    PRIVATE_BUILD_DESC="kuntao_row-user 7.0 NRD90N P2a42_S251_171107_ROW release-keys" 
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Lenovo/kuntao_row/P2a42:7.0/NRD90N/P2a42_S251_171107_ROW:user/release-keys"
