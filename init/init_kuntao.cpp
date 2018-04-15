2966e34acfa068a0ef293139491aaef69b3d538f
GSandeep <14311A0431@sreenidhi.edu.in>
2018-04-15 19:37:06 +0530

    Fix derp

--- init/init_kuntao.cpp
+++ init/init_kuntao.cpp
@@ -30,6 +30,9 @@
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
 #include <android-base/file.h>
 #include <android-base/properties.h>
 #include <android-base/strings.h>
