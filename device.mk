466d00e63e6e84e74c417c9f301da62197a7f811
GSandeep <14311A0431@sreenidhi.edu.in>
2018-04-22 19:40:22 +0530

    Add vendor display java

--- device.mk
+++ device.mk
@@ -153,8 +153,12 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm 
-    
+    vendor.lineage.livedisplay@1.0-service-sdm \
+    vendor.lineage.livedisplay-V1.0-java
+
+PRODUCT_BOOT_JARS += \
+    vendor.lineage.livedisplay-V1.0-java
+
 # Graphic HAL
 PRODUCT_PACKAGES += \
      android.hardware.graphics.allocator@2.0-impl \
