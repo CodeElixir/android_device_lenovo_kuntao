87640c3716a7429fa4079ca493ea6d1d04983a32
GSandeep <14311A0431@sreenidhi.edu.in>
2018-04-23 15:01:01 +0530

    Binderize Hals

--- device.mk
+++ device.mk
@@ -101,9 +101,12 @@ PRODUCT_PACKAGES += \
     tinymix 
 
 PRODUCT_PACKAGES += \
-     android.hardware.audio@2.0-impl \
-     android.hardware.audio.effect@2.0-impl \
-     android.hardware.soundtrigger@2.0-impl
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service 
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
@@ -131,6 +134,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	camera.device@1.0-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
@@ -160,6 +164,7 @@ PRODUCT_PACKAGES += \
      android.hardware.graphics.allocator@2.0-impl \
      android.hardware.graphics.allocator@2.0-service \
      android.hardware.graphics.composer@2.1-impl \
+     android.hardware.graphics.composer@2.1-service \
      android.hardware.graphics.mapper@2.0-impl \
      android.hardware.configstore@1.0-service \
 
