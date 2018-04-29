396fff147d8270e679e289c507404a894493c252
GSandeep <14311A0431@sreenidhi.edu.in>
2018-04-29 18:39:51 +0530

    Fix buid error

--- device.mk
+++ device.mk
@@ -450,8 +450,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Seccomp policy
  PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy 
+    
 # Vendor
 $(call inherit-product, vendor/lenovo/kuntao/kuntao-vendor.mk)
